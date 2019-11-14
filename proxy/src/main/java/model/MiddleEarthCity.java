f591afeecbbfa6dc3c608b597e9c4809d1dcfec6
ViniKroth <vinicius.kroth@acad.pucrs.br>
2019-11-13 22:37:01 -0300

    add proxy implementation

---
+++ proxy/src/main/java/model/MiddleEarthCity.java
@@ -0,0 +1,6 @@
+package model;
+
+public interface MiddleEarthCity {
+
+	String enter(Passanger passanger);
+}
---
+++ proxy/src/main/java/type/Races.java
@@ -0,0 +1,16 @@
+package type;
+
+public class Races {
+
+	public static final String dwarf = "DWARF";
+
+	public static final String elf = "ELF";
+
+	public static final String hobbit = "HOBBIT";
+
+	public static final String human = "HUMAN";
+
+	public static final String orc = "ORC";
+
+	public static final String wizard = "WIZARD";
+}
